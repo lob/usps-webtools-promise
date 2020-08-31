0c441517b17c862c77ba97dbbe49897ae72908b5
Kyle McNally <kyle@kmcnally.net>
2020-08-31 14:21:25 -0400

    Refactor request

--- src/request.ts
+++ src/request.ts
@@ -5,29 +5,29 @@ import { create } from "xmlbuilder2";
 // can be used independently of AWS
 export default async (
   options: string | https.RequestOptions | URL
-): Promise<{}> => {
-  return new Promise((resolve, reject) => {
-    // This is the actual request
-    const request = https.request(options, (response) => {
-      let body = "";
-      response.on("data", (chunk) => (body += chunk));
-      response.on("end", () => {
-        try {
-          const xml = create(body);
-          // console.log(body);
-          resolve(xml.end({ format: "object" }));
-        } catch (error) {
-        }
-      });
-    });
-
-    // This deals with errors
-    request.on("error", (error) => {
+) => new Promise((resolve) => {
+  // This is the actual request
+  const request = https.request(options, (response) => {
+    let body = "";
+    // eslint-disable-next-line no-return-assign
+    response.on("data", (chunk) => (body += chunk));
+    response.on("end", () => {
+      try {
+        const xml = create(body);
+        // console.log(body);
+        resolve(xml.end({ format: "object" }));
+      }
+      catch (error) {
         throw new Error(error);
+      }
     });
+  });
 
-    // This ends the request
-    request.end();
+  // This deals with errors
+  request.on("error", (error) => {
     throw new Error(JSON.stringify(error));
   });
-};
+
+  // This ends the request
+  request.end();
+});
