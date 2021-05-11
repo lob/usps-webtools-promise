c48dd6062bef3504901ef6830abf5f9f98829c8f
Kyle McNally <kyle@kmcnally.net>
2021-05-11 15:08:01 -0400

    update test

--- test/pricing-rate.js
+++ test/pricing-rate.js
@@ -1,12 +1,6 @@
 const test = require("ava");
 const USPS = require("../dist/usps").default;
 
-// Load .env
-if (process.env.NODE_ENV !== "production") {
-  // eslint-disable-next-line global-require
-  require("dotenv").config({ path: "test/.env" });
-}
-
 const usps = new USPS({
   userId: process.env.USPS_ID,
 });
@@ -22,8 +16,6 @@ test("Zipcode Lookup should return the address with zip", async (t) => {
       Ounces: 6,
       Container: "FLAT RATE ENVELOPE",
       Pounds: 0,
-
-
     },
   });
   console.log(rate);
